12365844f7017a6d3da748c464d364bcabc85075
dinesh5866 <sdinesh6604@gmail.com>
2024-07-14 16:54:52 +0530

    modified the file

--- control.js
+++ control.js
@@ -1,1 +1,17 @@
+let color = document.getElementsByClassName("blue")
 
+function blue(){
+    document.getElementsByClassName("blue")[0].style.backgroundColor = "blue";
+}
+function green(){
+    document.getElementsByClassName("green")[0].style.backgroundColor = "green";
+}
+function red(){
+    document.getElementsByClassName("red")[0].style.backgroundColor = "red";
+}
+function reset(){
+    for(let i=0;i<color.length;i++){
+        color[i].style.backgroundColor = "white"
+        color[i].style.Color = "black"
+    }
+}
